fb2e818c66a7fbb7a965698ee08febc17fa397cd
TheRedPirates4661 <35805875+TheRedPirates4661@users.noreply.github.com>
2018-02-10 01:35:02 +0200

    Added numbers to buttons

--- src/org/usfirst/frc/team4661/robot/OI.java
+++ src/org/usfirst/frc/team4661/robot/OI.java
@@ -16,22 +16,22 @@ public class OI {
 		leftJoystick = new Joystick(RobotMap.LEFT_JOYSTICK_PORT);
 		rightJoystick = new Joystick(RobotMap.RIGHT_JOYSTICK_PORT);
 		if (SmartDashboard.containsKey(StringConsts.CLIMB_UP_BUTTON)) {
-			SmartDashboard.putNumber(StringConsts.CLIMB_UP_BUTTON, 1);
+			SmartDashboard.putNumber(StringConsts.CLIMB_UP_BUTTON, 2);
 		}
 		if (SmartDashboard.containsKey(StringConsts.CLIMB_DOWN_BUTTON)) {
-			SmartDashboard.putNumber(StringConsts.CLIMB_DOWN_BUTTON, 1);
+			SmartDashboard.putNumber(StringConsts.CLIMB_DOWN_BUTTON, 3);
 		}
 		if (SmartDashboard.containsKey(StringConsts.LIFT_UP_BUTTON)) {
-			SmartDashboard.putNumber(StringConsts.LIFT_UP_BUTTON, 1);
+			SmartDashboard.putNumber(StringConsts.LIFT_UP_BUTTON, 4);
 		}
 		if (SmartDashboard.containsKey(StringConsts.LIFT_DOWN_BUTTON)) {
-			SmartDashboard.putNumber(StringConsts.LIFT_DOWN_BUTTON, 1);
+			SmartDashboard.putNumber(StringConsts.LIFT_DOWN_BUTTON, 5);
 		}
 		if (SmartDashboard.containsKey(StringConsts.GRIP_CLOSE_BUTTON)) {
-			SmartDashboard.putNumber(StringConsts.GRIP_CLOSE_BUTTON, 1);
+			SmartDashboard.putNumber(StringConsts.GRIP_CLOSE_BUTTON, 4);
 		}
 		if (SmartDashboard.containsKey(StringConsts.GRIP_OPEN_BUTTON)) {
-			SmartDashboard.putNumber(StringConsts.GRIP_OPEN_BUTTON, 1);
+			SmartDashboard.putNumber(StringConsts.GRIP_OPEN_BUTTON, 5);
 		}
 		if (SmartDashboard.containsKey(StringConsts.DRIVE_STRAIGHT_BUTTON)) {
 			SmartDashboard.putNumber(StringConsts.DRIVE_STRAIGHT_BUTTON, 1);
@@ -47,28 +47,28 @@ public class OI {
 				(int) SmartDashboard.getNumber(StringConsts.DRIVE_STRAIGHT_BUTTON, 3));
 		rotate.whileHeld(new RotateByJoystick());
 		
-		JoystickButton liftUp = new JoystickButton(leftJoystick,
-				(int) SmartDashboard.getNumber(StringConsts.LIFT_UP_BUTTON, 1));
+		JoystickButton liftUp = new JoystickButton(rightJoystick,
+				(int) SmartDashboard.getNumber(StringConsts.LIFT_UP_BUTTON, 4));
 		liftUp.whileHeld(new Move(Robot.lift,99, (short) 1));
 
-		JoystickButton liftDown = new JoystickButton(leftJoystick,
-				(int) SmartDashboard.getNumber(StringConsts.LIFT_DOWN_BUTTON, 1));
+		JoystickButton liftDown = new JoystickButton(rightJoystick,
+				(int) SmartDashboard.getNumber(StringConsts.LIFT_DOWN_BUTTON, 5));
 		liftDown.whileHeld(new Move(Robot.lift,99, (short) -1));
 
 		JoystickButton climbUp = new JoystickButton(leftJoystick,
-				(int) SmartDashboard.getNumber(StringConsts.CLIMB_UP_BUTTON, 1));
+				(int) SmartDashboard.getNumber(StringConsts.CLIMB_UP_BUTTON, 2));
 		climbUp.whileHeld(new Move(Robot.climber,99, (short) 1));
 
 		JoystickButton climbDown = new JoystickButton(leftJoystick,
-				(int) SmartDashboard.getNumber(StringConsts.CLIMB_DOWN_BUTTON, 1));
+				(int) SmartDashboard.getNumber(StringConsts.CLIMB_DOWN_BUTTON, 3));
 		climbDown.whileHeld(new Move(Robot.climber,99, (short) -1));
 
 		JoystickButton closeGrip = new JoystickButton(leftJoystick,
-				(int) SmartDashboard.getNumber(StringConsts.GRIP_CLOSE_BUTTON, 1));
+				(int) SmartDashboard.getNumber(StringConsts.GRIP_CLOSE_BUTTON, 4));
 		closeGrip.whileHeld(new Move(Robot.climber,99, (short) 1));
 
 		JoystickButton openGrip = new JoystickButton(leftJoystick,
-				(int) SmartDashboard.getNumber(StringConsts.GRIP_OPEN_BUTTON, 1));
+				(int) SmartDashboard.getNumber(StringConsts.GRIP_OPEN_BUTTON, 5));
 		openGrip.whileHeld(new Move(Robot.climber,99, (short) -1));
 
 	}
