1984f1b4c1f6360ecd13cd79eacf6ce061807421
TheRedPirates4661 <35805875+TheRedPirates4661@users.noreply.github.com>
2018-02-10 02:03:05 +0200

    Changed port numbers and added a placeholder for the gyro channel number

--- src/org/usfirst/frc/team4661/robot/RobotMap.java
+++ src/org/usfirst/frc/team4661/robot/RobotMap.java
@@ -9,11 +9,11 @@ public class RobotMap {
 	public final static int LEFT_FRONT_TALON_PORT = 3;
 	public final static int RIGHT_REAR_TALON_PORT = 1;
 	public final static int RIGHT_FRONT_TALON_PORT = 0;
-	public final static int LIFT_TALON_PORT = 0;
-	public final static int CLIMB_UP_TALON_PORT = 0;
+	public final static int LIFT_TALON_PORT = 4;
+	public final static int CLIMB_UP_TALON_PORT = 1;
 	public final static int CLIMB_DOWN_TALON_PORT = 0;
-	public final static int LEFT_GRIP_TALON_PORT = 0;
-	public final static int RIGHT_GRIP_TALON_PORT = 0;
+	public final static int LEFT_GRIP_TALON_PORT = 3;
+	public final static int RIGHT_GRIP_TALON_PORT = 2;
 	// MicroSwitch Ports
 	public final static int LIFT_MAX_LIMIT = 0;
 	public final static int LIFT_MIN_LIMIT = 0;
@@ -21,4 +21,7 @@ public class RobotMap {
 	public final static int CLIMB_MIN_LIMIT =0;
 	public final static int GRIP_MAX_LIMIT =0;
 	public final static int GRIP_MIN_LIMIT =0;
+	//Gyro Channels
+	public final static int DRIVING_SYSTEM_GYRO =0;
 }
+
